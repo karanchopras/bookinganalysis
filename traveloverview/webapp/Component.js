7db95c810be0814f177950747a2295f316db5418
karanchopras <karanchopras@gmail.com>
2021-10-25 18:03:50 +0200

    1

--- traveloverview/webapp/Component.js
+++
@@ -1,7 +0,0 @@
-sap.ui.define(["sap/ovp/app/Component"], function(AppComponent) {
-    return AppComponent.extend("sap.fe.demo.traveloverview.Component", {
-        metadata: {
-            manifest: "json"
-        }
-    });
-});
